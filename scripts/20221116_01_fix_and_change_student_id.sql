05c5bd472018654e695d55d02eb381e62c648325
Surasith Taokok <taokok@gmail.com>
2022-11-17 10:12:26 +0700

    Fixed - change student id by SQL script

---
+++ scripts/20221116_01_fix_and_change_student_id.sql
@@ -0,0 +1,14 @@
+
+-- From 09177 to 09189 เด็กหญิงธนวรรณ ตาปราบ - DONE on prod
+call update_student_id('2565','3','1','09177','09189');
+
+
+-- From 05048 to 09177 เด็กชายธนาวิน บุตะเขียว - DONE on prod
+call update_student_id('2565','3','2','05048','09177');
+
+
+-- From 09186 to 09215 เด็กหญิงชมพู่ ดามะนาว - DONE on prod
+call update_student_id('2565','3','2','09186','09215');
+
+-- From 09112 to 09216 เด็กหญิงชลดา สาระวัน - Done on prod
+call update_student_id('2565','3','2','09112','09216');
