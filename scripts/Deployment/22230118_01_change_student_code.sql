f8fd2072a8b73ab2945a40220ea14d5251dc5f6c
Toby MacOS <taokok@gmail.com>
2023-01-19 01:38:21 +0700

    Fixed - data integrity (แก้ไขเลขประจำตัวนักเรียนให้ถูกต้อง)

---
+++ scripts/Deployment/22230118_01_change_student_code.sql
@@ -0,0 +1,16 @@
+
+
+
+-- ธนพร เหล่ามะลึก
+call update_student_id('2565','3','1','05036','09162');
+
+
+-- ณัฐพล
+call update_student_id('2565','4','1','00000','09210');
+
+
+-- remove students
+DELETE FROM students WHERE ID = '09219';
+
+DELETE FROM students WHERE ID = '09218';
+
