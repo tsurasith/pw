426906b5f3edd20298109dedf0ead0f86bc3bbf4
Toby MacOS <taokok@gmail.com>
2023-02-22 13:44:40 +0700

    Fixed - user permission module Curriculum

--- administrator/module_curriculum/index.php
+++ administrator/module_curriculum/index.php
@@ -54,6 +54,7 @@
                         $_SESSION['username'] == "tc101" || 
                         $_SESSION['username'] == "tc102" || 
                         $_SESSION['username'] == "tc103" ||
+						$_SESSION['username'] == "tc110" ||
                         $_SESSION['username'] == "tc137" || $_SESSION['username'] == "tc111" ||
                         $_SESSION['username'] == "tc144" || $_SESSION['username'] == "tc113" 
                     ) { ?>
